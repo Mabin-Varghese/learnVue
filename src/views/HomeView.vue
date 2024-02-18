6d3861f0e313ab815d3793031b7c1a962234ee5f
Mabin-Varghese <mabinvarghese23@gmail.com>
2024-02-18 22:38:13 +0530

    Add : scss added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <h1>Hello world</h1>
   </main>
 </template>
